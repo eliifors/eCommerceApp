a0e6478e049e70b6c5ef2ab3009f320c3fa49295
eliifors <elif.ors@ogr.sakarya.edu.tr>
2025-11-20 18:06:28 +0300

    feat: add node-gyp-build package and bcrypt dependency

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,20 @@
-
 require("dotenv").config();
 const express = require("express");
-const app = express();
 
 const mongoose = require("mongoose");
 const connectDB = require("./config/database");
-connectDB();
+const  authRoutes = require("./routes/auth.route")
 
 const bcrypt = require("bcrypt")
+
+
+const app = express();
+connectDB();
+
+app.use(express.json());
+
+//Routes
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 8000;
 app.listen (PORT,()=>{
